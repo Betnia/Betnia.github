50425b14e477fcde98611205245db7dee0d978c2
Betty Ehinmoro <98239555+Betnia@users.noreply.github.com>
2022-07-21 16:11:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+//Arrays
+var names = ["Jane", "John", "Joe", "James"];
+console.log(names);
+
+for (var i = 0; i < names.length; i++) {
+  console.log("Goodbye" + names[i]);
+}
+
+var names2 = ["Emily", "Michael", "Betty"];
+
+for (var i = 0; i < names2.length; i++) {
+  console.log("Hello " + names2[i]);
+};
+
+for (var name in names2) {
+  console.log("Hello " + names2[name]);
+}
+
+names2.greeting = "Goodbye!";
+
+for (var name in names2) {
+  console.log("Hello " + names2[name]);
+}
 
