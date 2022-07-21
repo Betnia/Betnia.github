b084381576da6488cd15a12dcfc57286ae981938
Betty Ehinmoro <98239555+Betnia@users.noreply.github.com>
2022-07-21 23:18:42 +0100

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,1 +1,8 @@
-
+(function () {
+console.log();
+  var Hellospeaker = {};
+  Hellospeaker.name ="Hello";
+  Hellospeaker.sayHello = function() {
+    console.log("Hello" + Hellospeaker.name);
+  }
+})();
