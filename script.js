9e496222b49b6ce2c7b4be54c7e07313456a0bd8
Betty Ehinmoro <98239555+Betnia@users.noreply.github.com>
2022-07-24 00:37:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 (function () {
-  
-  
+   
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
@@ -13,8 +12,7 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker(names[i]);
   }
 }
-  
- 
+   
 })();
 
 
