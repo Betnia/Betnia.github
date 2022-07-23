212acdfa803d9e578015e7b453e2a97e149d8d13
Betty Ehinmoro <98239555+Betnia@users.noreply.github.com>
2022-07-24 00:29:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
   
-  if (firstLetter ==='j') {
+  if (firstLetter === 'j') {
     byeSpeaker(names[i]);
   } 
   else {
-    Speaker(names[i]);
+    helloSpeaker(names[i]);
   }
 }
   
