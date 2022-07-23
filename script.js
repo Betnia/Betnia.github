7191caaf4e84b55f58db582c218642bd9699efd4
Betty Ehinmoro <98239555+Betnia@users.noreply.github.com>
2022-07-23 22:57:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-//Arrays
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-console.log(names);
-  
-for (var i = 0; i < names.length; i++) {
-  console.log("Goodbye " + names[i]);
-}
-
-for (var i = 0; i < names.length; i++) {
-  console.log("Hello " + names[i]);
-}
 (function () {
-console.log();
-  var GoodbyeSpeaker = {};
-  GoodbyeSpeaker.name ="Goodbye";
-  GoodbyeSpeaker.sayGoodbye= function() {
-    console.log("Goodbye" + GoodbyeSpeaker.name);
-  }
-  Window.GoodbyeSpeaker = helloSpeaker;
-})(Window);
   
-  (function () {
-console.log();
-  var Hellospeaker = {};
-  Hellospeaker.name ="Hello";
-  Hellospeaker.sayHello = function() {
-    console.log("Hello" + Hellospeaker.name);
-  }
-    Window.helloSpeaker = helloSpeaker;
-})(Window);
+  
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
- var firstLetter = names[i].charAt(0).toLowerCase();
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  
   if (firstLetter ==='j') {
-    GoodbyeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
+    byeSpeaker(names[i]);
+  } 
+  else {
+    Speaker(names[i]);
   }
+}
+  
+ 
+})();
 
 
